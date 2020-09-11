c5803d2b09e1f99d6abc83c53455f5a860db1dbd
Nic Barnes <colorpulse@gmail.com>
2020-09-12 00:55:17 +0200

    added reducer, store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from "react";
+import { Store } from "./Store";
+import { IEpisode, IAction } from "./interfaces";
 
-export default function App() {
+export default function App(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+
+  const fetchDataAction = async () => {
+    const URL =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+    const data = await fetch(URL);
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes,
+    });
+  };
+
+  const toggleFavAction = (episode: IEpisode): IAction => {
+    const episodeInFav = state.favorites.includes(episode);
+    let dispatchObj = {
+      type: "ADD_FAV",
+      payload: episode,
+    };
+    if (episodeInFav) {
+      const favWithoutEpisode = state.favorites.filter(
+        (fav: IEpisode) => fav.id !== episode.id
+      );
+      dispatchObj = {
+        type: "REMOVE_FAV",
+        payload: favWithoutEpisode,
+      };
+    }
+
+    return dispatch(dispatchObj);
+  };
   return (
     <>
-      <h1> Rick and Morty</h1>
-      <p>Pick your favorite episode!!</p>
+      <header className="header">
+        <div>
+          <h1> Rick and Morty</h1>
+          <p>Pick your favorite episode!! </p>
+        </div>
+
+        <ul>
+          Favorite Episodes:
+          {state.favorites.map((fav: IEpisode) => {
+            return <li>{fav.name}</li>;
+          })}
+        </ul>
+      </header>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => {
+          return (
+            <section key={episode.id} className="episode-box">
+              <img
+                src={episode.image.medium}
+                alt={`rick and morty ${episode.name}`}
+              ></img>
+              <div>{episode.name}</div>
+              <section>
+                <div>
+                  Season: {episode.season} NUmber: {episode.number}
+                </div>
+                <button type="button" onClick={() => toggleFavAction(episode)}>
+                  {state.favorites.find(
+                    (fav: IEpisode) => fav.id === episode.id
+                  )
+                    ? "Un fav"
+                    : "Fav this"}
+                </button>
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </>
-  )
+  );
 }
- 
